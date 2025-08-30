cc8fd1a8e96797b7c42c6deb50ddf7ca45fb146e
ThierryYama <thierryyocassino@gmail.com>
2025-08-30 18:22:44 -0300

    chore: Usando rotas.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,26 +1,20 @@
 import express, {Request, Response, ErrorRequestHandler} from 'express';
 import { PrismaClient } from '../src/generated/prisma';
 import cors from 'cors';
+import RotaCategoria from './routes/RotaCategoria';
 
 const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', //Frontend URL 
+    origin: 'http://localhost:5173',
     credentials: true,
 }));
 
 app.use(express.json());
 
-app.get('/api/categoria', async (req: Request, res: Response) => {
-  try {
-    const categorias = await prisma.categoria.findMany();
-    res.json(categorias);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar categorias' });
-  }
-});  
+app.use(RotaCategoria);
 
 app.use((req: Request, res: Response) => {
     res.status(404).json({ error: 'Endpoint não encontrado.' });
@@ -28,7 +22,7 @@ app.use((req: Request, res: Response) => {
 
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.error(err); // Exibe o erro no console
+    console.error(err); 
     res.status(400).json({ error: 'Ocorreu algum erro.' });
 };
 
