a8522cd9ce8e38019d3c9688bd3cc59c8ef49ae2
ThierryYama <thierryyocassino@gmail.com>
2025-09-03 19:36:29 -0300

    chore: Configuração shadcn e tailwind.

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
+    host: "::",
+    port: 5173,
     watch: {
       usePolling: true,
     },
-    host: true, // Garante que o servidor seja acessível externamente
-  }
-})
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
