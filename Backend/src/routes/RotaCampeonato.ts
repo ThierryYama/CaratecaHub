f7938972df82b7a1aae715284a6b096b94a7e2b3
ThierryYama <thierryyocassino@gmail.com>
2025-09-15 16:08:44 -0300

    chore: Novas rotas adicionadas

--- Backend/src/routes/RotaCampeonato.ts
+++ Backend/src/routes/RotaCampeonato.ts
@@ -9,8 +9,10 @@ router.put('/atualizarCampeonato/:id', CampeonatoController.atualizarCampeonato)
 router.delete('/deletarCampeonato/:id', CampeonatoController.deletarCampeonato);
 router.get('/listarCampeonatos', CampeonatoController.listarCampeonatos);
 router.get('/listarCampeonatosPorIdDeAssociacao/:id', CampeonatoController.listarCampeonatoPorIdDeAssociacao);
+router.get('/listarCampeonato/:id', CampeonatoController.listarCampeonatoPorId);
 router.post('/adicionarCategoriaAoCampeonato/:idCampeonato', CampeonatoController.adicionarCategoriaAoCampeonato);
 router.delete('/removerCategoriaDeCampeonato/:idCampeonato/:idCategoria', CampeonatoController.removerCategoriaDeCampeonato);
 router.get('/listarCategoriasDeCampeonato/:idCampeonato', CampeonatoController.listarCategoriasDeCampeonato);
+router.put('/atualizarEnderecoCampeonato/:idCampeonato', CampeonatoController.atualizarEnderecoCampeonato);
 
 export default router;
