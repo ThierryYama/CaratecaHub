59729e959f944b4cb3fdae925cb7a1dbe68dbd1b
ThierryYama <thierryyocassino@gmail.com>
2025-09-17 17:25:21 -0300

    chore: Componente atualizado.

--- Frontend/src/components/ui/badge.tsx
+++ Frontend/src/components/ui/badge.tsx
@@ -26,6 +26,8 @@ const badgeVariants = cva(
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {
+  className?: string
+  variant?: 'default' | 'secondary' | 'destructive' | 'outline'
   children?: React.ReactNode
 }
 
