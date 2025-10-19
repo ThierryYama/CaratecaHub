d91f69fcb3bf1d15f72062e85e18270df3981053
ThierryYama <thierryyocassino@gmail.com>
2025-10-19 16:03:44 -0300

    feat: impl das rotas protegidas.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Campeonatos from "./pages/Campeonatos";
@@ -22,6 +21,7 @@ import Inscricoes from "./pages/Inscricoes";
 import CampeonatosPublicos from "./pages/CampeonatosPublicos";
 import InscreverAtletas from "./pages/InscreverAtletas";
 import { SidebarProvider } from "./context/SidebarContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -35,20 +35,20 @@ const App = () => (
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Index />} /> 
-            <Route path="/campeonatos" element={<Campeonatos />} /> 
-            <Route path="/campeonatos-publicos" element={<CampeonatosPublicos />} /> 
-            <Route path="/inscrever-atletas/:id" element={<InscreverAtletas />} /> 
-            <Route path="/meu-campeonato/:id" element={<MeuCampeonato />} /> 
-            <Route path="/meu-campeonato/:id/modalidades" element={<VincularCategorias />} /> 
-            <Route path="/meu-campeonato/:id/inscricoes" element={<Inscricoes />} /> 
-            <Route path="/categorias" element={<Categorias />} /> 
-            <Route path="/atletas" element={<Atletas />} /> 
-            <Route path="/equipes" element={<Equipes />} /> 
-            <Route path="/perfil" element={<Perfil />} /> 
-            <Route path="/meu-campeonato/:id/chaveamentos" element={<ChaveamentosGerencia />} /> 
-            <Route path="/meu-campeonato/:id/historico" element={<HistoricoBrackets />} /> 
-            <Route path="/meu-campeonato/:id/resultados" element={<ResultadosFinais />} /> 
+            <Route path="/" element={<ProtectedRoute><Campeonatos /></ProtectedRoute>} /> 
+            <Route path="/campeonatos" element={<ProtectedRoute><Campeonatos /></ProtectedRoute>} /> 
+            <Route path="/campeonatos-publicos" element={<ProtectedRoute><CampeonatosPublicos /></ProtectedRoute>} /> 
+            <Route path="/inscrever-atletas/:id" element={<ProtectedRoute><InscreverAtletas /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id" element={<ProtectedRoute><MeuCampeonato /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id/modalidades" element={<ProtectedRoute><VincularCategorias /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id/inscricoes" element={<ProtectedRoute><Inscricoes /></ProtectedRoute>} /> 
+            <Route path="/categorias" element={<ProtectedRoute><Categorias /></ProtectedRoute>} /> 
+            <Route path="/atletas" element={<ProtectedRoute><Atletas /></ProtectedRoute>} /> 
+            <Route path="/equipes" element={<ProtectedRoute><Equipes /></ProtectedRoute>} /> 
+            <Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id/chaveamentos" element={<ProtectedRoute><ChaveamentosGerencia /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id/historico" element={<ProtectedRoute><HistoricoBrackets /></ProtectedRoute>} /> 
+            <Route path="/meu-campeonato/:id/resultados" element={<ProtectedRoute><ResultadosFinais /></ProtectedRoute>} /> 
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
